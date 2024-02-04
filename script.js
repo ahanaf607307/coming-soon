90f7ca89421953a5ed8997a0a99f29e5dbac56ae
Ahanaf Mubasshir Rishad <123632951+ahanaf607307@users.noreply.github.com>
2024-02-04 14:52:49 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+var countDownDate = new Date("Feb 21, 2024 00:00:00").getTime();
+
+var x = setInterval(function () {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("min").innerHTML = minutes;
+  document.getElementById("sec").innerHTML = seconds;
+
+  if (distance < 0) {
+    document.getElementById("days").innerHTML = "00";
+    document.getElementById("hours").innerHTML = "00";
+    document.getElementById("min").innerHTML = "00";
+    document.getElementById("sec").innerHTML = "00";
+  }
+}, 1000);
