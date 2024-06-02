64944dc55ffff43153dbb90c28d8955d5000d7d6
Ahanaf Mubasshir Rishad <123632951+ahanaf607307@users.noreply.github.com>
2024-06-02 13:53:22 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("May 29, 2024 00:00:00").getTime();
+var countDownDate = new Date("May 29, 2025 00:00:00").getTime();
 
 var x = setInterval(function () {
   var now = new Date().getTime();
